31ab9fa28cc6c732317a64d45da7d418b448691a
eloisaper <eloisaperuzzo2000@outlook.com>
2023-05-02 11:34:03 -0300

    alteracao2

--- BANCO/projeto.sql
+++ BANCO/projeto.sql
@@ -12,3 +12,9 @@ create table usuario
     fone_usuario varchar(30) null,
 	senha varchar(30) not null
 );
+
+-- inserindo dados
+--insert into usuario
+--(nome_usuario, fone_usuario, email_usuario, senha)
+--values ('bianca','(18)99132446458','cas6@mg88.com','18163');
+--select * from usuario;
---
+++ cadastrousuario.php
@@ -0,0 +1,22 @@
+<?php
+    include("conexao.php");
+    //para copiar todo o código do arq indicado e colar aqui;
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $fone = $_POST['fone'];
+    $senha = $_POST['senha'];
+
+    echo "Nome: $nome";
+    echo "Email: $email";
+    echo "Fone: $fone";
+    echo "Senha: $senha";
+
+    $sql = "insert into usuario (nome_usuario, email_usuario, fone_ususario, senha)";
+    $sql .= "values ('".$nome."','".$email."','".$fone."','".$senha."')";
+    echo $sql. "<br>";
+    $result = mysqli_query($con, $sql);
+    if($result)
+        echo "Dados cadastrados com sucesso";
+    else
+        echo "erro ao tenatar cadastrar";
+?>
